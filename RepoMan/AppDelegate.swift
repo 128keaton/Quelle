ac084bd2baa12ea709577bd797224918bf737431
Keaton Burleson <keaton.burleson@me.com>
2017-01-02 22:44:08 -0600

    Initial Commit

---
+++ RepoMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RepoMan
+//
+//  Created by Keaton Burleson on 1/2/17.
+//  Copyright © 2017 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RepoMan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RepoMan
+//
+//  Created by Keaton Burleson on 1/2/17.
+//  Copyright © 2017 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
